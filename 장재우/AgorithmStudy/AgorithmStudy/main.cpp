ae0e2853fb6291623035872ffc12157a3d51cd33
Arkady1356 <arkady1356@naver.com>
2022-05-16 16:15:25 +0900

    0516 백준 풀이

---
+++ 장재우/AgorithmStudy/AgorithmStudy/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+extern int problem2445(void);
+extern int problem1924(void);
+extern int problem1929(void);
+
+int main() {
+	//problem2445();
+	//problem1924();
+	problem1929();
+
+	return 0;
+}
